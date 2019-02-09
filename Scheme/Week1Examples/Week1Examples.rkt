73042f346bcf8e34264b3855ce9407a5b3a26576
ABalev <sashobalev@abv.bg>
2019-02-09 09:00:19 +0200

    Update Week1Examples.rkt

--- Scheme/Week1Examples/Week1Examples.rkt
+++ Scheme/Week1Examples/Week1Examples.rkt
@@ -1,12 +1,48 @@
-; Find nth Fibonacci number.
+#lang racket
+
+;;; Task 1: Find nth Fibonacci number. 
+
 (define (fib n)
   (if (< n 2)
       n
       (+ (fib (- n 1)) (fib (- n 2)))))
 
-; Hanoi tower movement.
+;;; Task 2: Hanoi tower movement. 
+
 (define (move n from to spare)
   (cond ((= n 0) "Finish")
         (else
          (move (- n 1) from spare to)
          (move (- n 1) spare to from))))
+
+;;; Task 3: Method to implement different sums easily. 
+
+; Simple sum.
+(define (sum-int a b)
+  (if (> a b)
+      0
+      (+ a (sum-int (+ 1 a) b))))
+
+;; term and next are procedures.
+;; Function sum below can be used to implement other sum functions.
+(define (sum term a next b)
+  (if ( > a b)
+      0
+      (+ (term a)
+         (sum term
+              (next a)
+              next
+              b))))
+
+(define (1+ a)
+  (+ 1 a))
+
+(define (square a)
+  (* a a))
+
+(define (sum-int2 a b)
+  (define (identity a) a)
+  (sum identity a  1+ b))
+
+(define (sum-sq a b)
+  (sum square a 1+ b))
